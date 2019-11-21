7646d92959ef9496be181cde685e98641246b425
Hayden Roche <haydenroche5@gmail.com>
2019-11-20 19:51:25 -0600

    various changes and added conda_spec_file for recreating conda environment

--- train_detector.py
+++ train_detector.py
@@ -5,7 +5,19 @@ from io import BytesIO
 import numpy as np
 import requests
 import time
-from datetime import datetime
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('predictions.log')
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+formatter = logging.Formatter('[%(asctime)s] %(message)s', datefmt='%m-%d-%Y %H:%M:%S')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 width = 1920
 height = 1080
@@ -28,17 +40,15 @@ model = Sequential([
 model.load_weights('train_detection_cnn.h5')
 
 def detect_train(frame):
-    prediction = (model.predict(frame)).flatten()[0]
-    if prediction != 1.0:
-        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S');
-        print('{}: Prediction: {}'.format(current_time, prediction))
+    prediction = model.predict(frame).flatten()[0]
+    logger.info('Prediction: {}'.format(prediction))
 
 while (True):
-    response = requests.get('http://10.10.1.181/axis-cgi/jpg/image.cgi?resolution={}x{}'.format(width, height))
+    response = requests.get('http://10.10.1.181/axis-cgi/jpg/image.cgi?resolution={}x{}'.format(width, height), timeout=5)
     image = Image.open(BytesIO(response.content))
     image_resized = image.resize((scaled_width, scaled_height))
     frame = np.array(image_resized)
     frame_expanded = np.expand_dims(frame, axis=0)
     detect_train(frame_expanded)
 
-    time.sleep(2)
+    time.sleep(3)
