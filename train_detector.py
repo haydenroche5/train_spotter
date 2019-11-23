d779efea4763bc3fc8f88ff751caef42369e1f2d
Hayden Roche <haydenroche5@gmail.com>
2019-11-23 14:48:24 -0600

    a bunch of data and script improvements

--- train_detector.py
+++ train_detector.py
@@ -1,19 +1,20 @@
-from keras.models import Sequential
-from keras.layers import Conv2D, Dropout, MaxPooling2D, Dense, Flatten
 from PIL import Image
 from io import BytesIO
 import numpy as np
 import requests
 import time
 import logging
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from requests.exceptions import ConnectionError
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler('predictions.log')
+fh = logging.FileHandler('predictions.log', mode = 'w')
 fh.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.ERROR)
-formatter = logging.Formatter('[%(asctime)s] %(message)s', datefmt='%m-%d-%Y %H:%M:%S')
+formatter = logging.Formatter('[%(asctime)s] %(message)s', datefmt = '%m-%d-%Y %H:%M:%S')
 fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 logger.addHandler(fh)
@@ -21,34 +22,28 @@ logger.addHandler(ch)
 
 width = 1920
 height = 1080
-channels = 3
-scale_factor = 0.2
-scaled_width = int(width * scale_factor)
-scaled_height = int(height * scale_factor)
-num_filters = 16
-filter_size = 3
-pool_size = 2
 
-model = Sequential([
-  Conv2D(num_filters, filter_size, input_shape=(scaled_height, scaled_width, channels), activation='relu'),
-  Conv2D(num_filters, filter_size, activation='relu'),
-  MaxPooling2D(pool_size=pool_size),
-  Dropout(0.5),
-  Flatten(),
-  Dense(1, activation='sigmoid'),
-])
-model.load_weights('train_detection_cnn.h5')
+model = load_model('train_detection_cnn')
+input_img_height, input_img_width = model.layers[0].input_shape[1:3]
 
 def detect_train(frame):
-    prediction = model.predict(frame).flatten()[0]
-    logger.info('Prediction: {}'.format(prediction))
+    prediction_value = model.predict(frame).flatten()[0]
+    threshold = 0.98
+    if prediction_value > threshold:
+        logger.info('Prediction value: {}'.format(prediction_value))
+        logger.info('Prediction: train')
+    # else:
+    #     logger.info('Prediction: no_train')
 
 while (True):
-    response = requests.get('http://10.10.1.181/axis-cgi/jpg/image.cgi?resolution={}x{}'.format(width, height), timeout=5)
-    image = Image.open(BytesIO(response.content))
-    image_resized = image.resize((scaled_width, scaled_height))
-    frame = np.array(image_resized)
-    frame_expanded = np.expand_dims(frame, axis=0)
-    detect_train(frame_expanded)
-
-    time.sleep(3)
+    try:
+        response = requests.get('http://10.10.1.181/axis-cgi/jpg/image.cgi?resolution={}x{}'.format(width, height), timeout = 5)
+        image = Image.open(BytesIO(response.content))
+        image_resized = image.resize((input_img_width, input_img_height))
+        frame = np.array(image_resized)
+        frame_scaled = frame / 255.0
+        frame_scaled_expanded = np.expand_dims(frame_scaled, axis = 0)
+        detect_train(frame_scaled_expanded)
+        time.sleep(1)
+    except ConnectionError:
+        print('Timed out trying to get an image from the webcam.')
