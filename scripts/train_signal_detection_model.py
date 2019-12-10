592aa22758baa6d54b7955dfe59b0da7b63e3d78
Hayden Roche <haydenroche5@gmail.com>
2019-12-10 10:06:10 -0600

    signal detecton model stuff

--- scripts/train_signal_detection_model.py
+++ scripts/train_signal_detection_model.py
@@ -5,47 +5,46 @@ root_dir = dirname(dirname(abspath(__file__)))
 sys.path.append(root_dir)
 from vision.model import SignalDetectionModel
 
+import cv2
 import argparse
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import tensorflow as tf
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.models import load_model
 
 
 def main(args):
-    height = 1080
-    width = 1920
+    height = 130
+    width = 130
     num_channels = 3
-    scale_factor = 0.2
-    # TODO: Make these scaled down images square
-    scaled_height = int(height * scale_factor)
-    scaled_width = int(width * scale_factor)
-
     learning_rate = 1e-3
+    momentum = 0.9
 
-    optimizer = SGD(lr=learning_rate)
-    model = SignalDetectionModel.build(width=scaled_width,
-                                       height=scaled_height,
+    optimizer = SGD(lr=learning_rate, momentum=momentum)
+    model = SignalDetectionModel.build(width=width,
+                                       height=height,
                                        num_channels=num_channels)
     model.compile(loss='binary_crossentropy',
                   optimizer=optimizer,
                   metrics=['accuracy'])
     print(model.summary())
 
-    img_gen = ImageDataGenerator(rescale=1. / 255, validation_split=0.25)
+    img_gen = ImageDataGenerator(rescale=1. / 255, validation_split=0.1)
     training_generator = img_gen.flow_from_directory(
         args.data_dir,
-        target_size=(scaled_height, scaled_width),
+        target_size=(height, width),
         batch_size=args.batch_size,
         class_mode='binary',
         subset='training',
         shuffle=True)
     validation_generator = img_gen.flow_from_directory(
-        args.data_dir,  # same directory as training data
-        target_size=(scaled_height, scaled_width),
+        args.data_dir,
+        target_size=(height, width),
         batch_size=args.batch_size,
         class_mode='binary',
         subset='validation',
@@ -73,43 +72,14 @@ def main(args):
         validation_steps=math.ceil(num_validation_samples / args.batch_size))
 
     history_file_path = os.path.join(
-        args.output_dir, 'signal_detector_training_history.pickle')
+        args.output_dir, 'signal_detection_model_training_history.pickle')
     with open(history_file_path, 'wb') as history_file:
         pickle.dump(H.history, history_file)
-
-    epochs_array = np.arange(args.num_epochs)
-    val_loss_line = plt.plot(epochs_array,
-                             H.history['val_loss'],
-                             label='Validation Loss')
-    training_loss_line = plt.plot(epochs_array,
-                                  H.history['loss'],
-                                  label='Training Loss')
-    plt.setp(val_loss_line, linewidth=2.0, marker='+', markersize=10.0)
-    plt.setp(training_loss_line, linewidth=2.0, marker='4', markersize=10.0)
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.grid(True)
-    plt.legend()
-    plt.show()
-
-    val_accuracy_line = plt.plot(epochs_array,
-                                 H.history['val_accuracy'],
-                                 label='Validation Accuracy')
-    accuracy_line = plt.plot(epochs_array,
-                             H.history['accuracy'],
-                             label='Training Accuracy')
-    plt.setp(val_accuracy_line, linewidth=2.0, marker='+', markersize=10.0)
-    plt.setp(accuracy_line, linewidth=2.0, marker='4', markersize=10.0)
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.grid(True)
-    plt.legend()
-    plt.show()
 
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(
-        description='Retrain the signal detection model.')
+        description='Train the signal detection model.')
     arg_parser.add_argument('--data-dir',
                             dest='data_dir',
                             required=True,
