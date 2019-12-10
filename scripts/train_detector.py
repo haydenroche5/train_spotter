02ba89a843484e30ee8f5c407b80fc214ec56570
Hayden Roche <haydenroche5@gmail.com>
2019-12-10 10:06:48 -0600

    changes to train detector to use signal detector as well

--- scripts/train_detector.py
+++ scripts/train_detector.py
@@ -13,6 +13,62 @@ import os
 import re
 
 
+def get_camera_img(camera_ip, width, height):
+    response = requests.get(
+        'http://{}/axis-cgi/jpg/image.cgi?resolution={}x{}'.format(
+            args.camera_ip, width, height),
+        timeout=5)
+    img = Image.open(BytesIO(response.content))
+
+    return img
+
+
+def prepare_img_for_train_detection(img, model):
+    input_img_height, input_img_width = model.layers[0].input_shape[1:3]
+    img_resized = img.resize((input_img_width, input_img_height))
+    image_array = np.array(image_resized)
+    image_array_scaled = image_array / 255.0
+    image_array_scaled_expanded = np.expand_dims(image_array_scaled, axis=0)
+
+    return image_array_scaled_expanded
+
+
+def prepare_imgs_for_signal_detection(img, model):
+    signal_xs = [1090, 1218]
+    signal_ys = [306, 515]
+
+    input_img_height, input_img_width = model.layers[0].input_shape[1:3]
+    cropped_imgs = []
+    for x, y in zip(signal_xs, signal_ys):
+        cropped_img = img[y:y + input_img_height, x:x + input_img_width]
+        image_array = np.array(cropped_img)
+        image_array_scaled = cropped_img / 255.0
+        image_array_scaled_expanded = np.expand_dims(image_array_scaled,
+                                                     axis=0)
+        cropped_imgs.append(image_array_scaled_expanded)
+
+    return cropped_imgs
+
+
+def save_moment(event_dir, event_number, moment_number,
+                train_prediction_value):
+    images_path = os.path.join(event_dir, str(event_number), 'images')
+    if not os.path.exists(images_path):
+        os.makedirs(images_path)
+
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    img_path = os.path.join(images_path, '{}.jpg'.format(moment_number))
+    image_resized.save(img_path)
+    moment = {
+        'event_number': event_number,
+        'timestamp': timestamp,
+        'train_prediction_value': train_prediction_value,
+        'img_path': img_path,
+    }
+
+    return moment
+
+
 def main(args):
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
@@ -31,8 +87,11 @@ def main(args):
     height = 1080
     logger.info('THRESHOLD: {}'.format(args.threshold))
 
-    model = load_model(args.model_dir)
-    input_img_height, input_img_width = model.layers[0].input_shape[1:3]
+    train_detection_model = load_model(
+        os.path.join(args.model_dir, 'train_detection', 'model'))
+    signal_detection_model = load_model(
+        os.path.join(args.model_dir, 'signal_detection', 'model'))
+
     ongoing_event = False
     ongoing_event_moments = []
     moment_counter = 0
@@ -47,19 +106,22 @@ def main(args):
     images_path = None
     while True:
         try:
-            response = requests.get(
-                'http://{}/axis-cgi/jpg/image.cgi?resolution={}x{}'.format(
-                    args.camera_ip, width, height),
-                timeout=5)
-            image = Image.open(BytesIO(response.content))
-            image_resized = image.resize((input_img_width, input_img_height))
-            frame = np.array(image_resized)
-            frame_scaled = frame / 255.0
-            frame_scaled_expanded = np.expand_dims(frame_scaled, axis=0)
-            prediction_value = np.array(
-                model.predict_on_batch(frame_scaled_expanded)).flatten()[0]
+            img = get_camera_img(args.camera_ip, width, height)
+            train_detection_input_img = prepare_img_for_train_detection(img)
+            signal_detection_input_imgs = prepare_imgs_for_signal_detection(
+                img)
+
+            for img in signal_detection_input_imgs:
+                signal_prediction_value = np.array(
+                    model.predict_on_batch(img)).flatten()[0]
+                logger.info('Signal prediction value: {}'.format(
+                    signal_prediction_value))
+
+            train_prediction_value = np.array(
+                model.predict_on_batch(train_detection_input_img)).flatten()[0]
+
             if ongoing_event:
-                if prediction_value <= args.threshold:
+                if train_prediction_value <= args.threshold:
                     event_completion_progress += 1
                 else:
                     event_completion_progress = 0
@@ -80,42 +142,23 @@ def main(args):
                     ongoing_event_moments = []
                 else:
                     # TODO: factor out
-                    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-                    img_path = os.path.join(images_path,
-                                            '{}.jpg'.format(moment_counter))
-                    image_resized.save(img_path)
-                    moment_counter += 1
-                    event_moment = {
-                        'event_number': event_number,
-                        'timestamp': timestamp,
-                        'prediction_value': prediction_value,
-                        'img_path': img_path,
-                    }
-                    ongoing_event_moments.append(event_moment)
-                    logger.info(
-                        'Prediction value: {}'.format(prediction_value))
+                    moment = save_moment(args.event_dir, event_number,
+                                         moment_counter,
+                                         train_prediction_value)
+                    ongoing_event_moments.append(moment)
+                    logger.info('Train prediction value: {}'.format(
+                        train_prediction_value))
 
-            elif prediction_value > args.threshold:
+            elif train_prediction_value > args.threshold:
                 ongoing_event = True
-                images_path = os.path.join(args.event_dir, str(event_number),
-                                           'images')
-                os.makedirs(images_path)
-                timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-                img_path = os.path.join(images_path,
-                                        '{}.jpg'.format(moment_counter))
-                image_resized.save(img_path)
-                moment_counter += 1
-                event_moment = {
-                    'event_number': event_number,
-                    'timestamp': timestamp,
-                    'prediction_value': prediction_value,
-                    'img_path': img_path,
-                }
-                ongoing_event_moments.append(event_moment)
+                moment = save_moment(args.event_dir, event_number,
+                                     moment_counter, train_prediction_value)
+                ongoing_event_moments.append(moment)
                 logger.info('##############################')
                 logger.info('Begin event #{}'.format(event_number))
                 logger.info('##############################')
-                logger.info('Prediction value: {}'.format(prediction_value))
+                logger.info('Train prediction value: {}'.format(
+                    train_prediction_value))
             time.sleep(3)
         except ConnectionError:
             raise Exception(
@@ -128,7 +171,7 @@ if __name__ == '__main__':
     arg_parser.add_argument('--model-dir',
                             dest='model_dir',
                             required=True,
-                            help='Directory containing model.')
+                            help='Directory containing models.')
     arg_parser.add_argument('--event-dir',
                             dest='event_dir',
                             required=True,
