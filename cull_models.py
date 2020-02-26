3a6e057c321ed2f74c527c983c4e8b6430d844a0
Hayden Roche <haydenroche5@gmail.com>
2020-02-25 20:32:20 -0600

    finalize cull_models.py script

--- cull_models.py
+++ cull_models.py
@@ -1,7 +1,6 @@
 import os
 import argparse
 import re
-import shutil
 
 
 def main(args):
@@ -9,7 +8,13 @@ def main(args):
         os.path.join(args.model_dir, file)
         for file in os.listdir(args.model_dir) if file.endswith('.hdf5')
     ]
-    hdf5_names = [os.path.basename(f) for f in hdf5_files]
+    num_models = len(hdf5_files)
+
+    if args.n >= num_models:
+        print(
+            f'Nothing to do. N, {args.n}, >=    the number of model files, {num_models}.'
+        )
+        return
 
     for i, hdf5_file in enumerate(hdf5_files):
         hdf5_name = os.path.basename(hdf5_file)
@@ -23,9 +28,14 @@ def main(args):
             raise Exception(
                 '{} doesn\'t match the expected pattern.'.format(hdf5_name))
 
-    hdf5_files_sorted = sorted(hdf5_files, key=lambda tup: tup[1])
-    for f in hdf5_files_sorted:
-        print(f)
+    hdf5_files.sort(key=lambda x: x[1], reverse=True)
+    num_models_to_del = num_models - args.n
+
+    print(f'Will keep the top {args.n} models.')
+
+    for i in range(num_models_to_del):
+        print(f'Deleting {hdf5_files[i][0]}.')
+        os.remove(hdf5_files[i][0])
 
 
 if __name__ == '__main__':
