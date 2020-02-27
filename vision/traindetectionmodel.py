3100d4b7cc318b55b6cc30fe5c96e6a2183ab051
Hayden Roche <haydenroche5@gmail.com>
2020-02-27 17:34:31 -0600

    increase capacity again, and change kernel size from 3x3 to 5x5 for CONV layers

--- vision/traindetectionmodel.py
+++ vision/traindetectionmodel.py
@@ -5,25 +5,27 @@ from tensorflow.keras.layers import Conv2D, BatchNormalization, MaxPooling2D, Dr
 class TrainDetectionModel:
     @staticmethod
     def build(width, height, num_channels):
-        conv_kernel_size = (3, 3)
+        conv_kernel_size = (5, 5)
 
         model = Sequential()
         model.add(
-            Conv2D(4,
+            Conv2D(8,
                    conv_kernel_size,
                    input_shape=(height, width, num_channels),
-                   activation='relu'))
+                   activation='relu',
+                   padding='same'))
         model.add(BatchNormalization())
         model.add(MaxPooling2D())
         model.add(Dropout(0.3))
 
-        model.add(Conv2D(8, conv_kernel_size, activation='relu'))
+        model.add(
+            Conv2D(16, conv_kernel_size, activation='relu', padding='same'))
         model.add(BatchNormalization())
         model.add(MaxPooling2D())
         model.add(Dropout(0.3))
 
         model.add(Flatten())
-        model.add(Dense(8, activation='relu'))
+        model.add(Dense(16, activation='relu'))
         model.add(Dropout(0.5))
         model.add(Dense(1, activation='sigmoid'))
 
