fa0df40dbc6519b4f962c6eb8afcd1757a58915e
Hayden Roche <haydenroche5@gmail.com>
2020-03-23 17:04:54 -0500

    remove advanced data augmentation, for now

--- train_train_detection_model.py
+++ train_train_detection_model.py
@@ -33,13 +33,7 @@ def main(args):
 
     img_gen = ImageDataGenerator(rescale=1. / 255,
                                  validation_split=validation_split,
-                                 rotation_range=20,
-                                 zoom_range=0.15,
-                                 width_shift_range=0.2,
-                                 height_shift_range=0.2,
-                                 shear_range=0.15,
-                                 horizontal_flip=True,
-                                 fill_mode="nearest")
+                                 horizontal_flip=True)
     training_generator = img_gen.flow_from_directory(
         args.data_dir,
         target_size=(height, width),
