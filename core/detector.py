75c1caf45f6a9a3d0f50de6e0c83f767a53b0dbc
Hayden Roche <haydenroche5@gmail.com>
2020-03-23 17:04:06 -0500

    modularized detector design, use spotter.py instead of train_detector.py, which is being deprecated

--- core/detector.py
+++ core/detector.py
@@ -1,13 +1,145 @@
+from PIL import Image
+from io import BytesIO
+from datetime import datetime
+import requests
+from requests.exceptions import RequestException
+import time
 import numpy as np
+import logging
+import zmq
+import os
+import base64
+
+from rocheml.preprocessing.imagetoarraypreprocessor import ImageToArrayPreprocessor
+from rocheml.preprocessing.rescalepreprocessor import RescalePreprocessor
+from rocheml.preprocessing.resizepreprocessor import ResizePreprocessor
+from rocheml.preprocessing.croppreprocessor import CropPreprocessor
+from vision.traindetectionmodel import TrainDetectionModel
+from vision.signaldetectionmodel import SignalDetectionModel
 
 
 class Detector:
-    def __init__(self, model, preprocessors=[]):
-        self.model = model
-        self.preprocessors = preprocessors
+    def __init__(self, camera_ip, intersection, train_detection_model_weights,
+                 signal_detection_model_weights, camera_img_width,
+                 camera_img_height, log_file, zmq_context, zmq_endpoint,
+                 sleep_length):
+        self.camera_ip = camera_ip
+        self.camera_img_width = camera_img_width
+        self.camera_img_height = camera_img_height
+        self.socket = zmq_context.socket(zmq.PUB)
+        self.socket.bind(f'inproc://{zmq_endpoint}')
+        self.sleep_length = sleep_length
 
-    def detect(self, imgs):
-        for p in self.preprocessors:
-            imgs = p.preprocess(imgs)
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        logging_formatter = logging.Formatter(
+            '[%(asctime)s] %(name)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(logging_formatter)
+        self.logger.addHandler(file_handler)
 
-        return np.squeeze(self.model.predict_on_batch(np.stack(imgs)))
+        train_input_width = 384
+        train_input_height = 216
+        num_channels = 3
+
+        self.train_resize_preprocessor = ResizePreprocessor(
+            train_input_width, train_input_height)
+        self.img_to_array_preprocessor = ImageToArrayPreprocessor(
+            'channels_last')
+        self.rescale_preprocessor = RescalePreprocessor(255)
+
+        self.train_detection_model = TrainDetectionModel.build(
+            width=train_input_width,
+            height=train_input_height,
+            num_channels=num_channels)
+
+        if intersection == 'fourth':
+            signal_input_height = 130
+            signal_input_width = 130
+            signal_box_origins = [(1090, 306), (1218, 515)]
+            self.crop_preprocessors = [
+                CropPreprocessor(signal_box_origins[0], signal_input_width,
+                                 signal_input_height),
+                CropPreprocessor(signal_box_origins[1], signal_input_width,
+                                 signal_input_height)
+            ]
+        elif intersection == 'chestnut':
+            signal_input_height = 180
+            signal_input_width = 170
+            signal_box_origin = (int(
+                (315 / 1920.0) * self.camera_img_width), 0)
+            self.crop_preprocessors = [
+                CropPreprocessor(signal_box_origin, signal_input_width,
+                                 signal_input_height)
+            ]
+        else:
+            raise Exception(
+                'Unrecognized intersection: {}.'.format(intersection))
+
+        self.signal_detection_model = SignalDetectionModel.build(
+            width=signal_input_width,
+            height=signal_input_height,
+            num_channels=num_channels)
+
+        self.logger.info('Loading models.')
+        self.train_detection_model.load_weights(train_detection_model_weights)
+        self.signal_detection_model.load_weights(
+            signal_detection_model_weights)
+        self.logger.info('Loading complete.')
+
+    def run(self):
+        self.logger.info('Starting detector.')
+
+        while True:
+            try:
+                camera_response = requests.get(
+                    'http://{}/axis-cgi/jpg/image.cgi?resolution={}x{}'.format(
+                        self.camera_ip, self.camera_img_width,
+                        self.camera_img_height),
+                    timeout=5)
+                img = Image.open(BytesIO(camera_response.content))
+
+                train_img = self.train_resize_preprocessor.preprocess([img])[0]
+                train_img = self.img_to_array_preprocessor.preprocess(
+                    [train_img])[0]
+                train_img = self.rescale_preprocessor.preprocess([train_img
+                                                                  ])[0]
+                print(train_img.shape)
+                print(np.expand_dims(train_img, axis=0).shape)
+                train_prediction_value = np.squeeze(
+                    self.train_detection_model.predict_on_batch(
+                        np.expand_dims(train_img, axis=0)))
+
+                signal_img = self.img_to_array_preprocessor.preprocess([img
+                                                                        ])[0]
+                signal_prediction_values = []
+
+                for crop_pp in self.crop_preprocessors:
+                    signal_img_crop = crop_pp.preprocess([signal_img])[0]
+                    signal_prediction_values.append(
+                        np.squeeze(
+                            self.signal_detection_model.predict_on_batch(
+                                np.expand_dims(signal_img_crop, axis=0))))
+
+                signal_prediction_value = max(signal_prediction_values).astype(
+                    float)
+
+                resized_img = self.train_resize_preprocessor.preprocess([img
+                                                                         ])[0]
+                resized_img_bytes = BytesIO()
+                resized_img.save(resized_img_bytes, format="JPEG")
+                resized_img_payload = base64.b64encode(
+                    resized_img_bytes.getvalue())
+
+                predictions_payload = str.encode(
+                    f'{train_prediction_value:.8f}, {signal_prediction_value:.8f}'
+                )
+
+                self.socket.send_multipart(
+                    [predictions_payload, resized_img_payload])
+
+                time.sleep(self.sleep_length)
+            except RequestException:
+                self.logger.warn(
+                    'Failed to get image from the webcam. Will try again.')
---
+++ core/eventtracker.py
@@ -0,0 +1,120 @@
+import os
+import logging
+from PIL import Image
+from io import BytesIO
+from datetime import datetime
+import zmq
+import pickle
+import base64
+
+
+class EventTracker:
+    def __init__(self, threshold, zmq_context, zmq_endpoint, event_dir,
+                 log_file):
+        self.threshold = threshold
+        self.socket = zmq_context.socket(zmq.SUB)
+        self.socket.connect(f'inproc://{zmq_endpoint}')
+        self.socket.setsockopt_string(zmq.SUBSCRIBE,
+                                      "")  # TODO: see if can use single quotes
+        self.event_dir = event_dir
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        logging_formatter = logging.Formatter(
+            '[%(asctime)s] %(name)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+        file_handler = logging.FileHandler(log_file)
+
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(logging_formatter)
+        self.logger.addHandler(file_handler)
+
+        self.logger.info('Threshold: {}.'.format(self.threshold))
+
+    def save_moment(self, img, event_number, moment_number,
+                    train_prediction_value, signal_prediction_value):
+        images_path = os.path.join(self.event_dir, str(event_number), 'images')
+
+        if not os.path.exists(images_path):
+            os.makedirs(images_path)
+
+        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+        img_path = os.path.join(images_path, '{}.jpg'.format(moment_number))
+        img.save(img_path)
+        moment = {
+            'event_number': event_number,
+            'timestamp': timestamp,
+            'train_prediction_value': train_prediction_value,
+            'img_path': img_path,
+            'signal_prediction_value': signal_prediction_value,
+        }
+
+        return moment
+
+    def run(self):
+        ongoing_event = False
+        ongoing_event_moments = []
+        moment_counter = 0
+        event_complete_threshold = 5
+        event_completion_progress = 0
+        event_number = 0
+
+        if not os.path.exists(self.event_dir):
+            os.makedirs(self.event_dir)
+        else:
+            if os.listdir(self.event_dir):
+                event_number = max(
+                    [int(e) for e in os.listdir(self.event_dir)]) + 1
+
+        while True:
+            predictions, img_b64 = self.socket.recv_multipart()
+            train_prediction_value, signal_prediction_value = [
+                float(val) for val in predictions.decode().split(', ')
+            ]
+            img = Image.open(BytesIO(base64.b64decode(img_b64)))
+
+            if signal_prediction_value > self.threshold:
+                self.logger.info('Signal is on. Prediction value: {}.'.format(
+                    signal_prediction_value))
+
+            if ongoing_event:
+                if train_prediction_value <= self.threshold:
+                    event_completion_progress += 1
+                else:
+                    event_completion_progress = 0
+
+                if event_completion_progress > event_complete_threshold:
+                    ongoing_event = False
+                    event_file_path = os.path.join(self.event_dir,
+                                                   str(event_number),
+                                                   'moments.pickle')
+
+                    with open(event_file_path, 'wb') as event_file:
+                        pickle.dump(ongoing_event_moments, event_file)
+                    self.logger.info('##############################')
+                    self.logger.info('End event #{}'.format(event_number))
+                    self.logger.info('##############################')
+                    event_completion_progress = 0
+                    moment_counter = 0
+                    event_number += 1
+                    ongoing_event_moments = []
+                else:
+                    moment = save_moment(img, event_number, moment_counter,
+                                         train_prediction_value,
+                                         signal_prediction_value)
+                    ongoing_event_moments.append(moment)
+                    moment_counter += 1
+                    self.logger.info('Train prediction value: {}'.format(
+                        train_prediction_value))
+            elif train_prediction_value > self.threshold:
+                ongoing_event = True
+
+                moment = save_moment(img, event_number, moment_counter,
+                                     train_prediction_value,
+                                     signal_prediction_value)
+                ongoing_event_moments.append(moment)
+                moment_counter += 1
+                self.logger.info('##############################')
+                self.logger.info('Begin event #{}'.format(event_number))
+                self.logger.info('##############################')
+                self.logger.info('Train prediction value: {}'.format(
+                    train_prediction_value))
