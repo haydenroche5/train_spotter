648b29d848cb7b48088bce1a4202b4a1e3b4a892
Hayden Roche <haydenroche5@gmail.com>
2020-03-27 12:19:13 -0500

    deeper train model

--- vision/traindetectionmodel.py
+++ vision/traindetectionmodel.py
@@ -5,7 +5,7 @@ from tensorflow.keras.layers import Conv2D, BatchNormalization, MaxPooling2D, Dr
 class TrainDetectionModel:
     @staticmethod
     def build(width, height, num_channels):
-        conv_kernel_size = (5, 5)
+        conv_kernel_size = (3, 3)
 
         model = Sequential()
         model.add(
@@ -16,22 +16,34 @@ class TrainDetectionModel:
                    padding='same'))
         model.add(BatchNormalization())
         model.add(MaxPooling2D())
-        model.add(Dropout(0.3))
+        model.add(Dropout(0.2))
 
         model.add(
             Conv2D(16, conv_kernel_size, activation='relu', padding='same'))
         model.add(BatchNormalization())
         model.add(MaxPooling2D())
-        model.add(Dropout(0.3))
+        model.add(Dropout(0.2))
 
         model.add(
             Conv2D(32, conv_kernel_size, activation='relu', padding='same'))
         model.add(BatchNormalization())
         model.add(MaxPooling2D())
-        model.add(Dropout(0.3))
+        model.add(Dropout(0.2))
+
+        model.add(
+            Conv2D(64, conv_kernel_size, activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D())
+        model.add(Dropout(0.2))
+
+        model.add(
+            Conv2D(128, conv_kernel_size, activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D())
+        model.add(Dropout(0.2))
 
         model.add(Flatten())
-        model.add(Dense(32, activation='relu'))
+        model.add(Dense(128, activation='relu'))
         model.add(Dropout(0.5))
         model.add(Dense(1, activation='sigmoid'))
 
