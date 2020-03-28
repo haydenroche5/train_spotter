52dfbb5361d665a8c58436cd282b4dac53c345cb
Hayden Roche <haydenroche5@gmail.com>
2020-03-28 14:01:29 -0500

    make training script take hyperparameters from a config JSON file

--- train_train_detection_model.py
+++ train_train_detection_model.py
@@ -3,8 +3,8 @@ import sys
 from os.path import dirname, abspath
 from vision.traindetectionmodel import TrainDetectionModel
 import argparse
-import pickle
 import math
+import json
 from datetime import datetime
 import tensorflow as tf
 from tensorflow.keras.optimizers import SGD
@@ -17,18 +17,26 @@ def main(args):
     # config.gpu_options.allow_growth = True
     # session = tf.compat.v1.Session(config=config)
 
+    with open(args.config, 'r') as f:
+        config = json.load(f)
+
     raw_height = 1080
     raw_width = 1920
     num_channels = 3
     scale_factor = 0.20
     height = int(raw_height * scale_factor)
     width = int(raw_width * scale_factor)
-    learning_rate = 5e-4
-    decay = learning_rate / args.num_epochs
-    momentum = 0.9
-    validation_split = 0.15
+    learning_rate = config['learning_rate']
+    decay = learning_rate / config['epochs']
+    momentum = config['momentum']
+    validation_split = config['validation_split']
 
-    optimizer = SGD(lr=learning_rate, decay=decay, momentum=momentum)
+    if config['optimizer'] == 'SGD':
+        optimizer = SGD(lr=learning_rate, decay=decay, momentum=momentum)
+    else:
+        raise Exception('Unsupported optimizer: {}.'.format(
+            config['optimizer']))
+
     model = TrainDetectionModel.build(width=width,
                                       height=height,
                                       num_channels=num_channels)
@@ -37,23 +45,24 @@ def main(args):
                   metrics=['accuracy'])
     print(model.summary())
 
-    img_gen = ImageDataGenerator(rescale=1. / 255,
-                                 validation_split=validation_split,
-                                 width_shift_range=0.1,
-                                 height_shift_range=0.1,
-                                 fill_mode="nearest",
-                                 horizontal_flip=True)
+    img_gen = ImageDataGenerator(
+        rescale=1. / 255,
+        validation_split=validation_split,
+        width_shift_range=config['width_shift_range'],
+        height_shift_range=config['height_shift_range'],
+        fill_mode=config['fill_mode'],
+        horizontal_flip=config['horizontal_flip'])
     training_generator = img_gen.flow_from_directory(
         args.data_dir,
         target_size=(height, width),
-        batch_size=args.batch_size,
+        batch_size=config['batch_size'],
         class_mode='binary',
         subset='training',
         shuffle=True)
     validation_generator = img_gen.flow_from_directory(
         args.data_dir,
         target_size=(height, width),
-        batch_size=args.batch_size,
+        batch_size=config['batch_size'],
         class_mode='binary',
         subset='validation',
         shuffle=False)
@@ -67,7 +76,7 @@ def main(args):
 
     callbacks = [
         EarlyStopping(monitor='val_loss',
-                      patience=args.patience,
+                      patience=config['patience'],
                       restore_best_weights=True,
                       verbose=True),
         ModelCheckpoint(os.path.join(output_sub_dir,
@@ -78,13 +87,14 @@ def main(args):
         CSVLogger(os.path.join(output_sub_dir, 'epochs.csv'))
     ]
 
-    H = model.fit_generator(
-        training_generator,
-        steps_per_epoch=math.ceil(num_training_samples / args.batch_size),
-        epochs=args.num_epochs,
-        callbacks=callbacks,
-        validation_data=validation_generator,
-        validation_steps=math.ceil(num_validation_samples / args.batch_size))
+    H = model.fit_generator(training_generator,
+                            steps_per_epoch=math.ceil(num_training_samples /
+                                                      config['batch_size']),
+                            epochs=config['epochs'],
+                            callbacks=callbacks,
+                            validation_data=validation_generator,
+                            validation_steps=math.ceil(num_validation_samples /
+                                                       config['batch_size']))
 
 
 if __name__ == '__main__':
@@ -98,23 +108,8 @@ if __name__ == '__main__':
                             dest='output_dir',
                             required=True,
                             help='Directory to save training results in.')
-    arg_parser.add_argument('--num-epochs',
-                            dest='num_epochs',
-                            default=50,
-                            type=int,
-                            help='Number of epochs to train for.')
-    arg_parser.add_argument(
-        '--batch-size',
-        dest='batch_size',
-        default=32,
-        type=int,
-        help='Number of images to process in one training step.')
-    arg_parser.add_argument(
-        '--patience',
-        dest='patience',
-        default=3,
-        type=int,
-        help=
-        'Max number of epochs to train for without validation loss improvement.'
-    )
+    arg_parser.add_argument('--config',
+                            dest='config',
+                            required=True,
+                            help='Path to a JSON config file.')
     main(arg_parser.parse_args())
