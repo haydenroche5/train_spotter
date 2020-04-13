e765ee46fa6379c6b2951cc4de45a82d98a9544b
Hayden Roche <haydenroche5@gmail.com>
2020-04-13 18:32:30 -0500

    more startup script improvements

--- start_train_detector.sh
+++ start_train_detector.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# Clear the old stdout/stderr file
-> $stdout_stderr_file
+# Clear the startup.log file
+startup_log=/home/pi/train_spotter/startup.log
+> startup_log
 
 intersection=$(cat /home/pi/intersection | sed 's/ *$//')
 api_secret=$(cat /home/pi/api_secret | sed 's/ *$//')
 
 # Read configuration variables based on intersection file
 if [ "chestnut" == "$intersection" ]; then
-    source /home/pi/train_spotter/runtime_configs/chestnut_config.sh >> $stdout_stderr_file 2>&1
+    source /home/pi/train_spotter/runtime_configs/chestnut_config.sh >> $startup_log 2>&1
 elif [ "fourth" == "$intersection" ]; then
-    source /home/pi/train_spotter/runtime_configs/fourth_config.sh >> $stdout_stderr_file 2>&1
+    source /home/pi/train_spotter/runtime_configs/fourth_config.sh >> $startup_log 2>&1
 else
-    echo "Unrecognized intersection: $intersection." >> $stdout_stderr_file
+    echo "Unrecognized intersection: $intersection." >> $startup_log
     exit 1
 fi
 
 # Mount the USB drive
-mount $usb_device $usb_directory >> $stdout_stderr_file 2>&1
+mount $usb_device $usb_directory >> $startup_log 2>&1
 
 # Check if the spotter script is running
 if pgrep -f spotter > /dev/null 2>&1; then
-    echo "Train spotter is already running." >> $stdout_stderr_file
+    echo "Train spotter is already running." >> $startup_log
 else
     # TODO: remove --test when ready to go live
     su - pi -c "source $venv_file && \
@@ -36,7 +37,7 @@ else
     -l $sleep_length \
     -g $logging_directory \
     --api-secret $api_secret \
-    >> $stdout_stderr_file 2>&1 &"
+    > $stdout_stderr_file 2>&1 &"
 fi
 
 exit 0
