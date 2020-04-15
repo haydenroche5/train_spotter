f67115299d635a7ca16b5dafedff87f95fbcb335
Hayden Roche <haydenroche5@gmail.com>
2020-04-15 15:39:42 -0500

    simplify signal detection model

--- vision/signaldetectionmodel.py
+++ vision/signaldetectionmodel.py
@@ -9,30 +9,21 @@ from tensorflow.keras.layers import Dense
 class SignalDetectionModel:
     @staticmethod
     def build(width, height, num_channels):
-        max_pool_size = (2, 2)
-        max_pool_stride = 2
-        conv_kernel_size = (3, 3)
-
         model = Sequential()
         model.add(
-            Conv2D(32, (5, 5),
+            Conv2D(8, (3, 3),
                    input_shape=(height, width, num_channels),
-                   activation='relu'))
-        model.add(MaxPooling2D(pool_size=(3, 3), strides=max_pool_stride))
+                   activation='relu',
+                   padding='same'))
+        model.add(MaxPooling2D())
         model.add(Dropout(0.2))
 
-        model.add(Conv2D(32, conv_kernel_size, activation='relu'))
-        model.add(
-            MaxPooling2D(pool_size=max_pool_size, strides=max_pool_stride))
-        model.add(Dropout(0.2))
-
-        model.add(Conv2D(64, conv_kernel_size, activation='relu'))
-        model.add(
-            MaxPooling2D(pool_size=max_pool_size, strides=max_pool_stride))
+        model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+        model.add(MaxPooling2D())
         model.add(Dropout(0.2))
 
         model.add(Flatten())
-        model.add(Dense(64, activation='relu'))
+        model.add(Dense(16, activation='relu'))
         model.add(Dropout(0.5))
         model.add(Dense(1, activation='sigmoid'))
 
